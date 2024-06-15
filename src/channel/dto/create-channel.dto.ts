36fd6e8817094cb7bcaacc7af01f4678a422abdf
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-06-15 16:32:12 +0545

    move channel name regex to shared folder

--- src/channel/dto/create-channel.dto.ts
+++ src/channel/dto/create-channel.dto.ts
@@ -2,13 +2,18 @@ import {
   IsNotEmpty,
   IsNumber,
   IsString,
+  Matches,
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { Constants } from 'src/shared/constants/constants';
 
 export class CreateChannelDto {
   @MaxLength(16, { message: "Channel name can't be more than 16 characters." })
   @MinLength(3, { message: 'Channel name must be at least 3 characters.' })
+  @Matches(Constants.CHANNEL_NAME_REGEX, {
+    message: 'Channel name can only contain lowercase letters and hypens.',
+  })
   @IsString({ message: 'Channel name must be a string.' })
   @IsNotEmpty({ message: 'Channel name is required.' })
   name: string;
---
+++ src/shared/constants/constants.ts
@@ -0,0 +1,3 @@
+export const Constants = {
+  CHANNEL_NAME_REGEX: /^[a-z]+(-[a-z]+)*$/g,
+};
